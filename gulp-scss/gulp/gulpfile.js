bc87547a7f8c1a97e07e0370be7d1f71427684d1
daisuke <arai@rumcoke.net>
2020-03-21 05:14:34 +0900

    pretter追加

--- gulp-scss/gulp/gulpfile.js
+++ gulp-scss/gulp/gulpfile.js
@@ -7,7 +7,9 @@ let watch = require("gulp-watch");
 let plumber = require("gulp-plumber");
 let sass = require('gulp-sass');
 
+const prettier = require('gulp-prettier');
 
+const cached = require('gulp-cached');
 
 let browsers = ['last 2 versions', "ie >= 11"];
 // 最新2バージョンまでプレフィックス付与
@@ -39,7 +41,10 @@ let sassOptions = {
 gulp.task('scss', function () {
   return (
     gulp.src(paths.scss + '**/*.scss')
+      .pipe(cached('cache')) // ファイルをキャッシュさせて差分があるときのみbuild prettierでの変更をbuildし続けてしまうため
       .pipe(plumber())
+      .pipe(prettier({ singleQuote: true }))
+      .pipe(gulp.dest(file => file.base))
       .pipe(sass(sassOptions))
       .pipe(postcss([
         require('postcss-sorting')(sort_options),
